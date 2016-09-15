1b8b6d290c6256876fb91dfe3a170aa91db27d35
zhourunlai <u201316277@hust.edu.cn>
2016-09-15 15:51:25 +0800

    update

--- sklearn-sentiment-anylisis.py
+++ sklearn-sentiment-anylisis.py
@@ -1,42 +1,46 @@
-import os
-from sklearn.datasets import load_files
-from sklearn.cross_validation import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.grid_search import GridSearchCV
-from sklearn.svm import LinearSVC
-from sklearn import metrics
+# encoding=utf-8
 
-dataset_dir_name = os.getcwd() + '/txt_sentoken'
+import pandas as pd
+import jieba
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 
-# load data,and split into training/test set
-movie_reviews = load_files(dataset_dir_name)
+path = u'spide/output/疯狂动物城 Zootopia.txt'
+originData = pd.read_csv(path, sep='\t', header=None)
 
-# split
-doc_terms_train, doc_terms_test, doc_class_train, doc_class_test = train_test_split(
-    movie_reviews.data, movie_reviews.target, test_size=0.2, random_state=None)
+rateDocument = originData[1]
 
-# build a vectorizer/classifier pipeline
-pipeline = Pipeline([
-    ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-    ('clf', LinearSVC(C=1000)),
-])
+fiveRateDocument = rateDocument[originData[0] == 5]
+fourRateDocument = rateDocument[originData[0] == 4]
+threeRateDocument = rateDocument[originData[0] == 3]
+twoRateDocument = rateDocument[originData[0] == 2]
+oneRateDocument = rateDocument[originData[0] == 1]
 
-# grid search
-parameters = {
-    'vect__ngram_range': [(1, 1), (1, 2)],
-}
-grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
-grid_search.fit(doc_terms_train, doc_class_train)
-# print grid_search.grid_scores_
+posRateDocument = fiveRateDocument.append(fourRateDocument)
+negRateDocument = oneRateDocument.append(twoRateDocument)
+allRateDocument = posRateDocument.append(negRateDocument)
 
-# y_predicted
-y_predicted = grid_search.predict(doc_terms_test)
+corpus = []
+for pos in allRateDocument:
+    seg = jieba.cut(pos)
+    corpus.append(' '.join(seg))
 
-# report
-print metrics.classification_report(
-    doc_class_test, y_predicted, target_names=movie_reviews.target_names)
+# 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频
+vectorizer = CountVectorizer()
+# 该类会统计每个词语的tf-idf权值
+transformer = TfidfTransformer()
 
-# confusion matrix
-# confusion_matrix = metrics.confusion_matrix(doc_class_test, y_predicted)
-# print confusion_matrix
+# 第一个fit_transform是计算tf-idf，第二个fit_transform是将文本转为词频矩阵
+tf = vectorizer.fit_transform(corpus)
+tfidf = transformer.fit_transform(tf)
+
+# 获取词袋模型中的所有词语
+word = vectorizer.get_feature_names()
+# 将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重
+weight = tfidf.toarray()
+
+# 打印每类文本的tf-idf词语权重，第一个for遍历所有文本，第二个for便利某一类文本下的词语权重
+for i in range(len(weight)):
+    print u"-------这里输出第", i, u"类文本的词语tf-idf权重------"
+    for j in range(len(word)):
+        print word[j], weight[i][j]
