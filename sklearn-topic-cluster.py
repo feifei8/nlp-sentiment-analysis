99a6da8e4767a7c3c82eea1460c997fbb67b6aba
zhourunlai <u201316277@hust.edu.cn>
2016-09-18 23:28:50 +0800

    update

--- sklearn-topic-cluster.py
+++ sklearn-topic-cluster.py
@@ -33,13 +33,13 @@ if __name__ == "__main__":
     test_data = vectorizer.transform(test_words)
 
     # 聚类
-    # clf = KMeans(n_clusters=20)
+    clf = KMeans(n_clusters=20)
     # clf = DBSCAN(eps=0.5, min_samples=5, random_state=None)
-    clf = SpectralCoclustering(n_clusters=20, svd_method='arpack', random_state=0)
+    # clf = SpectralCoclustering(n_clusters=20, svd_method='arpack', random_state=0)
     clf.fit(train_data)
 
     # 中心点
-    # print(clf.cluster_centers_)
+    print(clf.cluster_centers_)
 
     # 每个样本所属的簇
     # print(clf.labels_)
@@ -56,35 +56,35 @@ if __name__ == "__main__":
     #     print i, words[i], clf.row_labels_[i - 1]
     #     i += 1
 
-    pca = PCA(n_components=3)  # 输出两维
-    newData = pca.fit_transform(train_data.toarray())  # 载入N维
-    print newData
-
-    x1 = []
-    y1 = []
-    i = 0
-    while i < 240:
-        x1.append(newData[i][0])
-        y1.append(newData[i][1])
-        i += 1
-
-    x2 = []
-    y2 = []
-    i = 240
-    while i < 480:
-        x2.append(newData[i][0])
-        y2.append(newData[i][1])
-        i += 1
-
-    x3 = []
-    y3 = []
-    i = 480
-    while i < 720:
-        x3.append(newData[i][0])
-        y3.append(newData[i][1])
-        i += 1
-
-    plt.plot(x1, y1, 'or')
-    plt.plot(x2, y2, 'og')
-    plt.plot(x3, y3, 'ob')
-    plt.show()
+    # pca = PCA(n_components=3)  # 输出两维
+    # newData = pca.fit_transform(train_data.toarray())  # 载入N维
+    # print newData
+    #
+    # x1 = []
+    # y1 = []
+    # i = 0
+    # while i < 240:
+    #     x1.append(newData[i][0])
+    #     y1.append(newData[i][1])
+    #     i += 1
+    #
+    # x2 = []
+    # y2 = []
+    # i = 240
+    # while i < 480:
+    #     x2.append(newData[i][0])
+    #     y2.append(newData[i][1])
+    #     i += 1
+    #
+    # x3 = []
+    # y3 = []
+    # i = 480
+    # while i < 720:
+    #     x3.append(newData[i][0])
+    #     y3.append(newData[i][1])
+    #     i += 1
+    #
+    # plt.plot(x1, y1, 'or')
+    # plt.plot(x2, y2, 'og')
+    # plt.plot(x3, y3, 'ob')
+    # plt.show()
